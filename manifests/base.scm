aa525ea73cb650db229b627fe0c3eb1f467ef0eb
Linas Vepstas <linasvepstas@gmail.com>
2025-12-15 08:26:15 +0000

    Profiles missing from the manifest

--- manifests/base.scm
+++ manifests/base.scm
@@ -1,6 +1,8 @@
 ;; Base manifest for AtomSpace development
 ;; Usage: guix shell -m manifests/base.scm
 
+(use-modules (guix profiles))
+
 (specifications->manifest
   '("cogutil"
     "atomspace"))
